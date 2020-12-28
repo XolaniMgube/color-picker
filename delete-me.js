f0d6465f1db109cae38fcedd4451116e5200add4
XolaniMgube <xnmgube@gmail.com>
2020-12-28 11:57:41 +0200

    changing variable names

---
+++ delete-me.js
@@ -0,0 +1,6 @@
+let nums = [{one: 1}, {two: 2}, {three: 3}]
+
+
+
+let xolani = nums.map((num) => num.one + 1)
+console.log(xolani)
--- js/main.js
+++ js/main.js
@@ -2,133 +2,72 @@
 
 "use strict";
 
-function targetColorCircles(event) {
+let colorImage = document.querySelectorAll(".color-image");
+let currentColor = document.querySelectorAll(".current-color");
+let total = 0;
+let agreeBtn = document.querySelector(".agree-button");
+let checkoutBtn = document.querySelector(".checkout");
+let addToCartBtn = document.querySelector(".add-to-cart");
+let details = document.querySelector(".details");
+
+// Function that targets the clicked color circles and changes relatives labels to the name of the clicked color
+let targetColorCircles = (event) => {
   let target = event.currentTarget;
-  let colorImage = document.querySelectorAll(".color-image");
-  let currentColor = document.querySelector(".current-color");
 
   for (let i = 0; i < colorImage.length; i++) {
     if (colorImage[i] == target) {
-      colorImage[i].classList.toggle("toggle-border");
-      currentColor.innerText = target.classList[1].toUpperCase();
+      colorImage[i].classList.toggle("toggle-border"); // Add a border to color circles
+      for (let j = 0; j < currentColor.length; j++) {
+        currentColor[j].innerText = target.classList[1].toUpperCase(); // Changes the value the color in the modal and COLOR:
+      }
     } else if (colorImage[i] != target) {
       colorImage[i].classList.remove("toggle-border");
     }
   }
-}
+};
 
-let minus = document.querySelector(".minus");
-let plus = document.querySelector(".plus");
-let selectedTotal = document.querySelector(".selected-total");
-let total = 0;
-let colorImage = document.querySelectorAll(".color-image");
-let chosenColor = document.querySelector(".chosen-color");
-let agree = document.querySelector(".agree");
-let checkout = document.querySelector(".checkout");
-let details = document.querySelector(".details");
-
+// Event listeners to adding and subtracting the quantity of the color on modal
 let plusMinus = () => {
-  for (let i = 0; i < colorImage.length; i++) {
-    colorImage[i].addEventListener('click', () => {
-      chosenColor.innerText = colorImage[i].classList[1].toUpperCase();
-    });
-  }
-
-  plus.addEventListener('click', () => {
+  let modalQuantity = document.querySelector(".modal-quantity");
+  let minus = document.querySelector(".minus");
+  let plus = document.querySelector(".plus");
+  plus.addEventListener("click", () => {
     total++;
-    selectedTotal.innerText = total;
+    modalQuantity.innerText = total;
   });
 
-  minus.addEventListener('click', () => {
-    total <= 0 ? total = 0 : total--
-    selectedTotal.innerText = total;
+  minus.addEventListener("click", () => {
+    total <= 0 ? (total = 0) : total--;
+    modalQuantity.innerText = total;
   });
-}
+};
 
-
+// Event listener for the Agree Button
 let agreeFunc = () => {
-  agree.addEventListener('click', () => {
-    let customFit = document.querySelector(".badge");
-    customFit.innerText = total;
-    document.querySelector(".add-to-cart").style.display = "none";
-    document.querySelector(".checkout").style.display = "block";
+  agreeBtn.addEventListener("click", () => {
+    let customFitQuantity = document.querySelector(".badge");
+    customFitQuantity.innerText = total;
+    addToCartBtn.style.display = "none";
+    checkoutBtn.style.display = "block";
   });
-}
-
+};
 
 let checkoutFunc = () => {
-  checkout.addEventListener('click', () => {
-    let checkoutColor = chosenColor.innerText.toLowerCase();
+  checkoutBtn.addEventListener("click", () => {
+    let checkoutColor = currentColor[0].innerText.toLowerCase();
     for (let x = 0; x < total; x++) {
       details.innerHTML += ` <div class="col-md-1 col-sm-1 col-2">
         <img src="./images/${checkoutColor}.png" class="color-image ${checkoutColor}" onclick="targetColorCircles(event)">
       </div>`;
     }
-    checkout.style.display = 'none';
+    checkoutBtn.style.display = "none";
   });
-}
+};
 
 let functionCalls = () => {
-  plusMinus()
-  agreeFunc()
-  checkoutFunc()
-}
+  plusMinus();
+  agreeFunc();
+  checkoutFunc();
+};
 
-functionCalls()
-
-
-
-
-// function addMinus() {
-//   let minus = document.querySelector(".minus");
-//   let plus = document.querySelector(".plus");
-//   let selectedTotal = document.querySelector(".selected-total");
-//   let total = 0;
-//   let colorImage = document.querySelectorAll(".color-image");
-//   let chosenColor = document.querySelector(".chosen-color");
-//   let agree = document.querySelector(".agree");
-
-  
-
-//   for (let i = 0; i < colorImage.length; i++) {
-//     colorImage[i].addEventListener("click", () => {
-//       chosenColor.innerText = colorImage[i].classList[1].toUpperCase();
-//     });
-//   }
-
-//   plus.addEventListener("click", () => {
-//     total = total + 1;
-//     selectedTotal.innerText = total;
-//   });
-
-//   minus.addEventListener("click", () => {
-//     if (total == 0) {
-//       total = 0;
-//     } else {
-//       total = total - 1;
-//     }
-//     selectedTotal.innerText = total;
-//   });
-
-//   agree.addEventListener("click", () => {
-//     let customFit = document.querySelector(".badge");
-//     customFit.innerText = total;
-//     document.querySelector(".add-to-cart").style.display = "none";
-//     document.querySelector(".checkout").style.display = "block";
-
-//     // console.log(customFit)
-//   });
-
-//   let checkout = document.querySelector(".checkout");
-//   let details = document.querySelector(".details");
-//   checkout.addEventListener("click", () => {
-//     let checkoutColor = chosenColor.innerText.toLowerCase();
-//     for (let x = 0; x < total; x++) {
-//       details.innerHTML += ` <div class="col-md-1 col-sm-1 col-2">
-//         <img src="./images/${checkoutColor}.png" class="color-image ${checkoutColor}" onclick="targetColorCircles(event)">
-//       </div>`;
-//     }
-//   });
-// }
-
-// addMinus();
+functionCalls();
