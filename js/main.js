c223d3a60051b8a287ec8ffed8e2524cf4b1c8bf
XolaniMgube <xnmgube@gmail.com>
2020-12-30 16:47:10 +0200

    code restructuring and cleaning

--- js/main.js
+++ js/main.js
@@ -3,12 +3,18 @@
 "use strict";
 
 let colorImage = document.querySelectorAll(".color-image");
-let currentColor = document.querySelectorAll(".current-color");
-let total = 0;
+let currentColor = document.querySelectorAll(".current-color"); 
 let agreeBtn = document.querySelector(".agree-button");
 let checkoutBtn = document.querySelector(".checkout");
 let addToCartBtn = document.querySelector(".add-to-cart");
 let modalQuantity = document.querySelector(".modal-quantity");
+let incrementBtn = document.querySelector(".increment");
+let decrementBtn = document.querySelector(".decrement");
+let fullPrice = document.querySelector("#full-price");
+let specialPrice = document.querySelector("#special-price");
+let details = document.querySelector(".details");
+let customFitQuantity = document.querySelector(".badge");
+let modalTable = document.querySelector("#summary-modal tbody");
 
 // Function that targets the clicked color circles and changes relatives labels to the name of the clicked color
 const targetColorCircles = (event) => {
@@ -29,9 +35,25 @@ const targetColorCircles = (event) => {
   checkoutBtn.style.display = "none";
 };
 
-let calcPrice = () => {
-  let fullPrice = document.querySelector("#full-price");
-  let specialPrice = document.querySelector("#special-price");
+// Adds 1 to modal Quantity
+const increment = () => {
+  incrementBtn.addEventListener("click", () => {
+    modalQuantity.innerText++;
+  });
+};
+
+// Subtracts 1 from modal Quantity
+const decrement = () => {
+  decrementBtn.addEventListener("click", () => {
+    modalQuantity.innerText <= 0
+      ? (modalQuantity.innerText = 0)
+      : modalQuantity.innerText--;
+  });
+};
+
+
+
+const calcPrice = () => {
   for (let i = 0; i < colorImage.length; i++) {
     colorImage[i].addEventListener("click", () => {
       fullPrice.innerText = colorImage[i].getAttribute("data-price");
@@ -40,55 +62,39 @@ let calcPrice = () => {
     });
   }
 };
-calcPrice();
 
-// Increment
-const increment = () => {
-  let incrementBtn = document.querySelector(".increment");
-  incrementBtn.addEventListener("click", () => {
-    modalQuantity.innerText++;
-  });
-};
 
-// Decrement
-const decrement = () => {
-  let decrementBtn = document.querySelector(".decrement");
-  decrementBtn.addEventListener("click", () => {
-    modalQuantity.innerText <= 0
-      ? (modalQuantity.innerText = 0)
-      : modalQuantity.innerText--;
-  });
-};
-
+// Adds circles to the details section
 const addDetailsCircles = () => {
   let checkoutColor = currentColor[0].innerText.toLowerCase();
-  let details = document.querySelector(".details");
-
   for (let x = 0; x < modalQuantity.innerText; x++) {
     details.innerHTML += ` <div class="col-md-1 col-sm-1 col-2"> 
         <img src="./images/${checkoutColor}.png" class="details-circles ${checkoutColor}">
-      </div>`; // Adding circles to the details section
+      </div>`; 
   }
 };
 
-const toggleButtons = () => {
-  let customFitQuantity = document.querySelector(".badge");
+// 
+const toggleButtons = () => {  
   let detailsLength = document.querySelectorAll(".details-circles").length;
   customFitQuantity.innerText = detailsLength;
   addToCartBtn.style.display = "none";
   checkoutBtn.style.display = "block";
 };
 
+
 const addToSummaryTable = () => {
   let checkoutColor = currentColor[0].innerText.toLowerCase();
-  let modalTable = document.querySelector("#summary-modal tbody");
-
   modalTable.innerHTML += `<tr>
       <td><img src="./images/${checkoutColor}.png" style="width: 20px;"></td>
-      <td>${checkoutColor}</td>
-      <td>R10</td>
+      <td class="not-bold">${checkoutColor}</td>
+      <td class="product-total not-bold"></td>
     </tr>`;
 };
+
+
+
+
 
 // Event listener for the Agree Button
 let agreeFunc = () => {
@@ -96,16 +102,15 @@ let agreeFunc = () => {
     addDetailsCircles();
     toggleButtons();
     addToSummaryTable();
-    modalQuantity.innerText = total;
+    modalQuantity.innerText = 0;
   });
 };
 
-
 let functionCalls = () => {
+  calcPrice();
   increment();
   decrement();
   agreeFunc();
-  checkoutFunc();
 };
 
 functionCalls();
